2567e7dd54ca198ff1aac54b584bb41e011cd2f3
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-11-17 11:52:14 +0100

    Update

--- app_main/src/pages/AgencyLogin.tsx
+++ app_main/src/pages/AgencyLogin.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -141,6 +142,7 @@ const AgencyLogin = () => {
           </Card>
         </div>
       </div>
+       <Footer />
     </div>
   );
 };
